a1bf4032e28ab8f2f3c5b9c8ce1a0bf48af68580
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2024-10-27 11:35:36 -0400

    Flatten array

--- src/chunk_data.rs
+++ src/chunk_data.rs
@@ -4,29 +4,34 @@ use crate::block::Block;
 
 pub const CHUNK_SIZE: usize = 32;
 
-#[derive(Clone, Copy, Default)]
-pub struct ChunkData([[[Block; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE]);
+#[derive(Clone, Copy)]
+pub struct ChunkData([Block; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE]);
+
+impl Default for ChunkData {
+    fn default() -> Self {
+        ChunkData([Block::Air; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE])
+    }
+}
 
 impl ChunkData {
     pub fn at(&self, x: usize, y: usize, z: usize) -> Block {
-        self.0[x][y][z]
+        self.0[Self::get_array_index(x, y, z)]
     }
 
     pub fn at_mut(&mut self, x: usize, y: usize, z: usize) -> &mut Block {
+        let index = Self::get_array_index(x, y, z);
         self.0
-            .get_mut(x)
-            .expect("x bounds")
-            .get_mut(y)
-            .expect("y bounds")
-            .get_mut(z)
-            .expect("z bounds")
+            .get_mut(index)
+            .expect("index bounds")
     }
 
     pub fn is_meshable(&self) -> bool {
         self.0
             .into_par_iter()
-            .flatten()
-            .flatten()
             .any(|v| v.is_meshable())
+    }
+
+    fn get_array_index(x: usize, y: usize, z: usize) -> usize {
+        CHUNK_SIZE * CHUNK_SIZE * x + CHUNK_SIZE * z + y
     }
 }
