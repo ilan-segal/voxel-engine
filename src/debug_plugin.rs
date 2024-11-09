501aa88956e304c3b434ed216a14fdcea6672580
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2024-11-09 13:17:39 -0500

    Toggle debug panel with F1

--- src/debug_plugin.rs
+++ src/debug_plugin.rs
@@ -3,6 +3,7 @@ use bevy::{
         EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin,
         SystemInformationDiagnosticsPlugin,
     },
+    input::{keyboard::KeyboardInput, ButtonState},
     prelude::*,
 };
 use iyes_perf_ui::{entries::PerfUiBundle, prelude::*};
@@ -24,7 +25,9 @@ impl Plugin for DebugPlugin {
         ))
         .add_perf_ui_simple_entry::<PerfUiCameraPosition>()
         .add_perf_ui_simple_entry::<PerfUiCameraFacing>()
-        .add_systems(Startup, setup);
+        .add_systems(Startup, setup)
+        .add_systems(Update, (toggle_debug_ui, update_debug_ui_visibility))
+        .init_resource::<DebugUiIsVisible>();
     }
 }
 
@@ -33,5 +36,42 @@ fn setup(mut commands: Commands) {
         PerfUiBundle::default(),
         PerfUiCameraPosition::default(),
         PerfUiCameraFacing::default(),
+        DebugUi,
     ));
+}
+
+#[derive(Component)]
+struct DebugUi;
+
+#[derive(Resource, Default)]
+struct DebugUiIsVisible(bool);
+
+fn update_debug_ui_visibility(
+    mut query: Query<&mut Visibility, With<DebugUi>>,
+    is_visible: Res<DebugUiIsVisible>,
+) {
+    for mut visibility in query.iter_mut() {
+        *visibility = if is_visible.0 {
+            Visibility::Visible
+        } else {
+            Visibility::Hidden
+        };
+    }
+}
+
+fn toggle_debug_ui(
+    mut key_events: EventReader<KeyboardInput>,
+    mut is_visible: ResMut<DebugUiIsVisible>,
+) {
+    for event in key_events.read() {
+        if event.state != ButtonState::Pressed {
+            return;
+        }
+        match event.key_code {
+            KeyCode::F1 => {
+                is_visible.0 = !is_visible.0;
+            }
+            _ => {}
+        }
+    }
 }
