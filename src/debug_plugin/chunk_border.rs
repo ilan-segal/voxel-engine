dd706a77d8e4adda7521bcaa7fcf61b9e10486e0
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2025-05-10 23:54:33 -0400

    Remove polyline usage for chunk border

--- src/debug_plugin/chunk_border.rs
+++ src/debug_plugin/chunk_border.rs
@@ -1,73 +1,39 @@
 use bevy::{color::palettes::css::BLUE, input::common_conditions::input_just_pressed, prelude::*};
-use bevy_polyline::prelude::{PolylineBundle, PolylineMaterial, PolylineMaterialHandle};
 
 use crate::{
     chunk::{position::ChunkPosition, CHUNK_SIZE},
-    cube_frame::{CubeFrameMeshHandle, CubeFrameSetup},
+    player::Player,
 };
 
 pub struct ChunkBorderPlugin;
 
 impl Plugin for ChunkBorderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup.after(CubeFrameSetup))
+        app.init_resource::<DrawChunkBorder>()
+            // .add_systems(Startup, setup.after(CubeFrameSetup))
             .add_systems(
                 Update,
                 (
-                    update_border_position,
+                    // update_border_position,
                     update_visibility.run_if(input_just_pressed(KeyCode::F5)),
+                    draw_border.run_if(resource_equals::<DrawChunkBorder>(DrawChunkBorder(true))),
                 ),
             );
     }
 }
 
-#[derive(Component)]
-struct ChunkBorder;
+#[derive(Resource, Default, PartialEq, Eq)]
+struct DrawChunkBorder(bool);
 
-fn setup(
-    mut commands: Commands,
-    mut polyline_materials: ResMut<Assets<PolylineMaterial>>,
-    mesh: Res<CubeFrameMeshHandle>,
-) {
-    let material = PolylineMaterialHandle(polyline_materials.add(PolylineMaterial {
-        width: 2.0,
-        color: BLUE.into(),
-        perspective: false,
-        depth_bias: -0.001,
-        ..default()
-    }));
-    commands.spawn((
-        ChunkBorder,
-        PolylineBundle {
-            polyline: mesh.0.clone(),
-            material,
-            transform: Transform::from_scale(CHUNK_SIZE as f32 * Vec3::ONE),
-            visibility: Visibility::Hidden,
-            ..Default::default()
-        },
-    ));
+fn update_visibility(mut res_draw: ResMut<DrawChunkBorder>) {
+    res_draw.0 = !res_draw.0;
 }
 
-fn update_border_position(
-    mut q_chunk_border: Query<&mut Transform, With<ChunkBorder>>,
-    q_camera_chunk_position: Query<&ChunkPosition, (With<Camera3d>, Changed<ChunkPosition>)>,
-) {
-    let mut border_transform = q_chunk_border
-        .get_single_mut()
-        .expect("There should be exactly one chunk border object");
-    let Ok(camera_chunk_pos) = q_camera_chunk_position.get_single() else {
-        return;
-    };
-    let offset = CHUNK_SIZE as f32 * 0.5 * Vec3::ONE;
-    border_transform.translation = offset + camera_chunk_pos.0.as_vec3() * CHUNK_SIZE as f32;
-}
-
-fn update_visibility(mut q_chunk_border_visibility: Query<&mut Visibility, With<ChunkBorder>>) {
-    let mut visibility = q_chunk_border_visibility
-        .get_single_mut()
-        .expect("There should be exactly one chunk border object");
-    *visibility = match *visibility {
-        Visibility::Hidden => Visibility::Visible,
-        _ => Visibility::Hidden,
-    };
+fn draw_border(mut gizmos: Gizmos, q_chunk_pos: Query<&ChunkPosition, With<Player>>) {
+    if let Ok(ChunkPosition(chunk_pos)) = q_chunk_pos.get_single() {
+        let translation = (chunk_pos.as_vec3() + Vec3::splat(0.5)) * CHUNK_SIZE as f32;
+        let transform =
+            Transform::from_translation(translation).with_scale(Vec3::splat(CHUNK_SIZE as f32));
+        gizmos.cuboid(transform, BLUE);
+    }
 }
